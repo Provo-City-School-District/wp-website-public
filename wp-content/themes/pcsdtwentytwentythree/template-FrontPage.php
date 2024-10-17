d59a9b5400197d1522610fadbc9a571393ba95a2
Josh Espinoza <joshespi@gmail.com>
2024-10-17 07:46:21 -0600

    correct IDs

--- wp-content/themes/pcsdtwentytwentythree/template-FrontPage.php
+++ wp-content/themes/pcsdtwentytwentythree/template-FrontPage.php
@@ -138,7 +138,7 @@ $get_to_know_fields = get_fields();
 			<div class="stories">
 				<?php
 				// excluding ID 1012. which is the Board Schedule.
-				$the_query = new WP_Query(array('posts_per_page' => 3, 'category__in' => array(1, 2), 'post_type'  => array('post', 'podcast')));
+				$the_query = new WP_Query(array('posts_per_page' => 3, 'category__in' => array(192, 1115), 'post_type'  => array('post', 'podcast')));
 				if ($the_query->have_posts()) :
 					while ($the_query->have_posts()) : $the_query->the_post(); ?>
 						<article>
