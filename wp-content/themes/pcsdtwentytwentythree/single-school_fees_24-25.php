1f1a5bf293d4b990329808f32d20b1ac68eb7499
Josh Espinoza <joshespi@gmail.com>
2024-10-21 11:35:48 -0600

    switch by category displays to table also

--- wp-content/themes/pcsdtwentytwentythree/single-school_fees_24-25.php
+++ wp-content/themes/pcsdtwentytwentythree/single-school_fees_24-25.php
@@ -3,12 +3,12 @@ get_header();
 ?>
 <main id="mainContent" class="sidebar">
 	<ol class="breadcrumbs" id="breadcrumbs">
-			<li><a href="https://provo.edu/">Home</a> / </li>
-			<li><a href="https://provo.edu/school-fees/">School Fees</a> / </li>
-			<li><a href="https://provo.edu/school_fees_24-25/">School Fees 24-25</a> / </li>
-			<li><?php single_post_title(); ?></li>
-		</ol>
-		<section id="currentPage">
+		<li><a href="https://provo.edu/">Home</a> / </li>
+		<li><a href="https://provo.edu/school-fees/">School Fees</a> / </li>
+		<li><a href="https://provo.edu/school_fees_24-25/">School Fees 24-25</a> / </li>
+		<li><?php single_post_title(); ?></li>
+	</ol>
+	<section id="currentPage">
 		<article class="activePost feePost">
 			<?php
 			?>
@@ -21,78 +21,66 @@ get_header();
 			<h1><?php single_post_title(); ?></h1>
 			<h2>Maximum Activity Fees</h2>
 			<section class="feeDisplay">
-				<article class="fee">
-					<p>Fee (not to exceed)</p>
-					<?php echo $fields['overall_activity_fee_amounts']['overall_activity_fee']; ?>
-				</article>
-				<article class="fee">
-					<p>Fundraising (not to exceed)</p>
-					<?php echo $fields['overall_activity_fee_amounts']['overall_course_fundraising']; ?>
-				</article>
-				<article class="fee">
-					<p>Total Maximum Fee</p>
-					<?php echo $fields['overall_activity_fee_amounts']['overall_course_total']; ?>
-				</article>
-				<article class="fee">
-					<p>Notes</p>
-					<?php echo $fields['overall_activity_fee_amounts']['notes']; ?>
-				</article>
+				<table>
+					<thead>
+						<tr>
+							<th>Fee (not to exceed)</th>
+							<th>Fundraising (not to exceed)</th>
+							<th>Total Maximum Fee</th>
+							<th>Notes</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><?php echo $fields['overall_activity_fee_amounts']['overall_activity_fee']; ?></td>
+							<td><?php echo $fields['overall_activity_fee_amounts']['overall_course_fundraising']; ?></td>
+							<td><?php echo $fields['overall_activity_fee_amounts']['overall_course_total']; ?></td>
+							<td><?php echo $fields['overall_activity_fee_amounts']['notes']; ?></td>
+						</tr>
+					</tbody>
+				</table>
 			</section>
 
 			<?php
-			//check that there are location specific fees
+			// Check that there are location specific fees
 			if ($fields['location_specific_fees']) {
 			?>
 				<h2>Breakdown of Fees By Location</h2>
 				<?php
-				//seperate each location into its own array
+				// Separate each location into its own array
 				foreach ($fields['location_specific_fees'] as $location) {
 				?>
 					<h3><?php echo $location['location']; ?></h3>
 					<section class="feeDisplay feeBreakdown">
-						<article class="fee">
-							<p>Fee Description</p>
-						</article>
-						<article class="fee">
-							<p>Fee</p>
-						</article>
-						<article class="fee">
-							<p>Fundraising</p>
-						</article>
-						<article class="fee">
-							<p>Total</p>
-						</article>
-						<article class="fee">
-							<p>Notes</p>
-						</article>
-						<article class="fee">
-							<p>Prior Year Approved Fee</p>
-						</article>
-						<?php
-						//display the actual fees
-						foreach ($location['breakdown_of_fees'] as $fee) {
-						?>
-							<article class="fee">
-								<span><?php echo $fee['fee_description']; ?></span>
-							</article>
-							<article class="fee">
-								<?php echo $fee['fee']; ?>
-							</article>
-							<article class="fee">
-								<?php echo $fee['fundraising']; ?>
-							</article>
-							<article class="fee">
-								<?php echo $fee['total']; ?>
-							</article>
-							<article class="fee">
-								<span><?php echo $fee['notes']; ?></span>
-							</article>
-							<article class="fee textright">
-								<span><?php echo $fee['prior_year_approved_fee']; ?></span>
-							</article>
-						<?php
-						} //end the breaddown_of_fees loop
-						?>
+						<table>
+							<thead>
+								<tr>
+									<th>Fee Description</th>
+									<th>Fee</th>
+									<th>Fundraising</th>
+									<th>Total</th>
+									<th>Notes</th>
+									<th>Prior Year Approved Fee</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								// Display the actual fees
+								foreach ($location['breakdown_of_fees'] as $fee) {
+								?>
+									<tr>
+										<td><?php echo $fee['fee_description']; ?></td>
+										<td><?php echo $fee['fee']; ?></td>
+										<td><?php echo $fee['fundraising']; ?></td>
+										<td><?php echo $fee['total']; ?></td>
+										<td><?php echo $fee['notes']; ?></td>
+										<td><?php echo $fee['prior_year_approved_fee']; ?></td>
+									</tr>
+								<?php
+								} // End the breakdown_of_fees loop
+								?>
+							</tbody>
+						</table>
 					</section>
 			<?php
 				} //end the location_specific_fees loop
