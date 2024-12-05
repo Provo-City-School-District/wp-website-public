0fbe0c8dc298173f49e78f877bfeb487aeeff859
Josh Espinoza <joshespi@gmail.com>
2024-12-05 10:37:12 -0700

    removing files that are no longer needed

--- wp-content/themes/pcsdtwentytwentythree/sidebar-equity.php
+++
@@ -1,9 +0,0 @@
-<aside id="rightSidebar" class="rightSidebar">
-	
-
-
-	<h2>Equity &amp; Diversity Contacts</h2>
-	<?php
-	echo file_get_contents('https://provo.edu/directory_page/student-services-equity-diversity/');
-	?>
-</aside>
